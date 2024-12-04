ca16c0e692ae761dab2a58dbd0398b901fd55878
Mohamadou Laminou <mohamadou055@gmail.com>
2024-12-04 23:41:19 +0100

    126 commit

--- public/main.js
+++ public/main.js
@@ -341,25 +341,25 @@ map.on("pointermove", function (evt) {
 
         // Gérer l'image ou l'icône par défaut
         const logoElement = document.getElementById("partner-logo");
-        const iconElement = document.getElementById("partner-logo-icon");
         const logoUrl = feature.get("img_logo");
 
         if (logoUrl) {
           // Vérifier si l'image est valide
           const img = new Image();
           img.onload = function () {
-            logoElement.src = logoUrl; // Charger l'image
+            logoElement.src = logoUrl; // Charger l'image si valide
             logoElement.style.display = "block"; // Afficher l'image
-            iconElement.style.display = "none"; // Cacher l'icône
+            document.getElementById("partner-logo-icon").style.display = "none"; // Cacher l'icône
           };
           img.onerror = function () {
             logoElement.style.display = "none"; // Cacher l'image
-            iconElement.style.display = "flex"; // Afficher l'icône
+            document.getElementById("partner-logo-icon").style.display =
+              "block"; // Afficher l'icône
           };
           img.src = logoUrl;
         } else {
           logoElement.style.display = "none"; // Cacher l'image
-          iconElement.style.display = "flex"; // Afficher l'icône
+          document.getElementById("partner-logo-icon").style.display = "block"; // Afficher l'icône
         }
 
         // Calculer la position initiale du popup

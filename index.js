be2fce1fece6f08ca872ff8b3727122b0a65787f
Mohamadou Laminou <mohamadou055@gmail.com>
2024-12-19 23:24:17 +0100

    137 commit

--- index.js
+++ index.js
@@ -136,7 +136,7 @@ app.get("/api/care_partner", async (req, res) => {
     const result = await pool.query(
       "SELECT jsonb_build_object('type', 'FeatureCollection', 'features', jsonb_agg(feature)) " +
         "FROM (SELECT jsonb_build_object('type', 'Feature', 'geometry', ST_AsGeoJSON(geom)::jsonb, " +
-        "'properties', to_jsonb(row) - 'geom') AS feature FROM (SELECT * FROM partenaire) row) features;"
+        "'properties', to_jsonb(row) - 'geom') AS feature FROM (SELECT * FROM partenaires) row) features;"
     );
     res.json(result.rows[0].jsonb_build_object);
   } catch (err) {

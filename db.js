89387869ed85e99f542ece4a543f28d34d0e6f9a
Mohamadou Laminou <mohamadou055@gmail.com>
2025-06-28 18:16:34 +0100

    Update for deployment

--- db.js
+++ db.js
@@ -1,12 +1,13 @@
+
 const { Pool } = require("pg");
 
 // Configuration de la connexion
 const pool = new Pool({
-  user: "postgres", 
-  host: "localhost", 
-  database: "care_project_db", 
-  password: "geopass", 
-  port: 5432, 
+  user: "postgres", // Nom d'utilisateur PostgreSQL
+  host: "db", // Hôte de votre base de données
+  database: "care_db", // Nom de votre base de données
+  password: "geopass", // Mot de passe PostgreSQL
+  port: 5432, // Port par défaut pour PostgreSQL
 });
 
 // Connexion à la base de données
@@ -16,7 +17,7 @@ pool.connect((err, client, release) => {
   }
   console.log("Connexion réussie à la base de données");
 
- 
+  // Exemple de requête simple pour vérifier la connexion
   client.query("SELECT NOW()", (err, result) => {
     release();
     if (err) {

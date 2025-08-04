7d78f10dc054b4d9cc5139cc6677b9d0bcbcc20a
Mohamadou Laminou <mohamadou055@gmail.com>
2025-08-04 05:04:57 +0100

    Update for observation 011

--- db.js
+++ db.js
@@ -1,8 +1,9 @@
 const { Pool } = require("pg");
+
 // Configuration de la connexion
 const pool = new Pool({
   user: "postgres", // Nom d'utilisateur PostgreSQL
-  host: "localhost", // Hôte de votre base de données
+  host: "db", // Hôte de votre base de données
   database: "care_db", // Nom de votre base de données
   password: "geopass", // Mot de passe PostgreSQL
   port: 5432, // Port par défaut pour PostgreSQL

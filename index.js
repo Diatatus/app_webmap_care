fc07b4938456feb7ba8379a2a8feaa18ead01287
Mohamadou Laminou <mohamadou055@gmail.com>
2025-08-20 06:15:43 +0100

    Update New config for deployment006

--- index.js
+++ index.js
@@ -302,5 +302,6 @@ app.get("/", (req, res) => {
 
 // Démarrage du serveur
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
+
